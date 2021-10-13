2a55d55d714b2f63c65625aacfb64d51d53659c5
Raunak173 <raunakagarwal173@gmail.com>
2021-10-13 12:28:04 +0530

    added coin details page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Components/Banner/Banner.js";
+import CoinsTable from "../Components/CoinsTable.js";
 
 const HomePage = () => {
   return (
     <div>
       <Banner />
+      <CoinsTable />
     </div>
   );
 };
